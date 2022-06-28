bbfe08f9c8ceba0b1a4ba74ef0aba05a833b3f78
Felix Brendel <felix@brendel.io>
2022-06-28 18:53:07 +0200

    asdas

--- src/displays/waveshare_2.9inch_epaper/examples/main.c
+++ src/displays/waveshare_2.9inch_epaper/examples/main.c
@@ -300,7 +300,7 @@ int init_font() {
         printf("original ptr: %lu\n", bit_ptr-font_data);
 
         /* bit_ptr += y_start * ((bmp_width_in_px % 8 == 0) ? bmp_width_in_px/8 : bmp_width_in_px/8 + 1) + 1; */
-        bit_ptr += y_start * (int)ceil(bmp_width_in_px/8.0)
+        bit_ptr += y_start * (int)ceil(bmp_width_in_px/8.0);
 
         printf("ptr + y: %lu\n", bit_ptr-font_data);
         bit_ptr += x_start / 8;
