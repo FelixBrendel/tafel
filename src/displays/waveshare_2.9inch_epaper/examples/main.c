782bcfbe18b519ae9aa1455970406e17b5ebb222
Felix Brendel <felix@brendel.io>
2022-06-29 00:06:02 +0200

    asdads

--- src/displays/waveshare_2.9inch_epaper/examples/main.c
+++ src/displays/waveshare_2.9inch_epaper/examples/main.c
@@ -211,7 +211,7 @@ int init_fonts() {
     stbtt_fontinfo font;
     unsigned char *bitmap;
 
-    FILE* font_file = fopen("./Sono-Medium.ttf", "rb");
+    FILE* font_file = fopen("./fonts/NanumGothicCoding-Regular.ttf", "rb");
     if (!font_file) {
         fprintf(stderr, "could not open ttf file");
         return 1;
