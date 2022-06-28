ae47e02bd681cd782ffd924701149ad4d15ce5fb
Felix Brendel <felix@brendel.io>
2022-06-28 18:19:45 +0200

    alsdkajsd

--- src/displays/waveshare_2.9inch_epaper/examples/main.c
+++ src/displays/waveshare_2.9inch_epaper/examples/main.c
@@ -330,7 +330,7 @@ int init_font() {
             /*     ++bit_ptr; */
             /* } */
 
-            if (x_offset+bmp_width_in_px % 8 != 0)
+            if ((x_offset+bmp_width_in_px) % 8 != 0)
                 ++bit_ptr;
 
             bit_ptr += (int)((char_width_in_px-x_end) / 8.0);
